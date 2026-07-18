92444ea690f40d64f3b0f92863cbe70721d496b9
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-07-18 13:23:54 +0530

    brhuuuuuyu

--- src/utils/loadGoogleFont.ts
+++ src/utils/loadGoogleFont.ts
@@ -36,14 +36,14 @@ async function loadGoogleFonts(
 > {
   const fontsConfig = [
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Geist Mono",
+      font: "Geist+Mono",
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Geist Mono",
+      font: "Geist+Mono",
       weight: 700,
       style: "bold",
     },
